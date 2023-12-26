94106e6700a4b10993e6d18ee085acaaf5cf5f51
MilaKot <146465048+MilaKot@users.noreply.github.com>
2023-12-26 22:37:55 +0300

    Add files via upload

---
+++ function increaseBalance(x, y) {.js
@@ -0,0 +1,16 @@
+function increaseBalance(x, y) {
+    return x + y;
+  }
+  function decreaseBalance(x, y) {
+    return x - y;
+  }
+  function divideBalanceByAccounts(x, y) {
+    return x / y;
+  }
+  function getRestAfterDivision(x, y) {
+    return x % y;
+  }
+  console.log(increaseBalance(3000, 700));
+  console.log(decreaseBalance(3000, 700));
+  console.log(divideBalanceByAccounts(3000, 2));
+  console.log(getRestAfterDivision(7000, 3));
